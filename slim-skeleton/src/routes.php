27eef0bc4d929150683e3eddb9d79b3271b94f88
kimura-tsuyoshi <maxim_4141.rock@zoho.com>
2017-03-10 02:39:14 +0900

    [add] get blog data

--- slim-skeleton/src/routes.php
+++ slim-skeleton/src/routes.php
@@ -1,4 +1,16 @@
 <?php
+// Get blog data
+$app->get('/blog', function ($request, $response, $args) {
+
+    $sql   = 'SELECT * FROM posts';
+    $query = $this->db->prepare($sql);
+    $query->execute();
+
+    $data = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    var_dump($data);
+});
+
 // Routes
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
