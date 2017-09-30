55478e6005d29b937e9c9bb8bb44284e5edb6733
kimura-tsuyoshi <maxim_4141.rock@zoho.com>
2017-09-30 19:04:54 +0900

    [modify] TwitterAPIの取得結果をMackerelAPIに登録する

--- slim-skeleton/src/routes/twitter.php
+++ slim-skeleton/src/routes/twitter.php
@@ -1,14 +1,20 @@
 <?php
 /**
- * Twitter用ルーティング設定
- */
+* Twitter用ルーティング設定
+*/
 $app->get('/twitter/search/', function ($request, $response, $args) {
     $params = [
-        'q'     => '艦これ',
-        //'count' => 5,
+        'q'     => '検索ワード',
+        'count' => 100,
     ];
     $tweets = $this->twitter->get('search/tweets', $params);
-    var_dump($tweets->search_metadata->count);
 
-    var_dump($this->mackerel->getHost('HOST_ID'));
+    $host = $this->mackerel->getHost('HOST_ID');
+    $metric = [
+        'hostId' => $host->id,
+        'time' => time(),
+        'name' => 'metric.twitter.検索ワード',
+        'value' => $tweets->search_metadata->count,
+    ];
+    $this->mackerel->postMetrics([$metric]);
 });
