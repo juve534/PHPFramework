176afda2e795762ec03ea4a79a14d53c6f26e0ef
kimura-tsuyoshi <maxim_4141.rock@zoho.com>
2017-12-28 00:01:12 +0900

    [add] Hello Worldの作成。

---
+++ dietcake/app/controllers/hello_controller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Class helloController
+ * @see AppController
+ * @package app.controllers
+ */
+class helloController extends AppController
+{
+    public function index()
+    {
+        ;
+    }
+}
